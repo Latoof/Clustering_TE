363dca8a0cc798c83aaa5d394008ede13b8ed445
latoof <latoof@myhost>
2012-05-09 21:46:39 +0200

    More algorithms
    Signed-off-by: latoof <latoof@myhost>

---
+++ Clustering_TE/src/Algorithms/CL_DENCLUE.java
@@ -0,0 +1,18 @@
+package Algorithms;
+
+import DataModel.DataSet;
+
+public class CL_DENCLUE<T> extends CL_algo<T> {
+
+	public CL_DENCLUE(DataSet d) {
+		super(d);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void runAlgo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Clustering_TE/src/Algorithms/MeterDistanceCalculator.java
@@ -0,0 +1,15 @@
+package Algorithms;
+
+import DataModel.DataElement;
+
+public class MeterDistanceCalculator implements DistanceCalculator {
+
+	@Override
+	public double calculateDistance(DataElement e1, DataElement e2) {
+		
+
+		
+		return 0;
+	}
+
+}
